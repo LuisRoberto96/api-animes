cabab3ef6241466c3b98dd3a00eea4545cdba1fa
LuisRoberto96 <luisfragosoh9@hotmail.com>
2022-01-08 16:11:35 -0300

    aplicação finalizada

---
+++ README.txt
@@ -0,0 +1,7 @@
+Projeto feito em React que realiza a busca de informações (utilização do método GET para a API) do site "kitsu.io".
+
+O projeto conta com a utilização das funções do useState e useEffect do React Hooks,
+bem como a utilização do Debounce para tratamento de requisições. 
+
+Cada pesquisa apresenta 12 itens na interface do projeto (foi estabelecido um limite de 12 itens por pesquisa).
+
--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect,useState} from 'react';
+import SearchInput from './SearchInput';
+import './App.css'
 
-function App() {
+const api = 'https://kitsu.io/api/edge/'
+
+export default function App() {
+  const [info, setInfo] = useState({}) 
+  const [text, setText] = useState('')
+  
+  useEffect(() => {
+    if (text) {
+      setInfo({})
+      console.log(text)
+      fetch(`${api}anime?filter[text]=${text}&page[limit]=12`)
+        .then ((response) => response.json())
+        .then ((response) => {
+          setInfo(response)
+          console.log(response)
+        })
+    }
+  }, [text])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Animes</h1>
+      <SearchInput 
+        value={text} 
+        onChange={(search) => setText(search)} 
+      />
+      <br />
+      {text && !info.data && (
+        <span>Carregando...</span>
+      )}
+      {info.data && (
+        <ul className="animes-list">
+          {info.data.map((anime) =>(
+            <li key={anime.id}>
+              <img 
+                src={anime.attributes.posterImage.small}
+                alt={anime.attributes.canonicalTitle} 
+              />
+              {anime.attributes.canonicalTitle}
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 }
-
-export default App;
